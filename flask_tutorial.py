5a69ebf608dbb9455d09ba33e174bd586d932649
Faisal Sayed <faisal@UM-Q07BFD59.attlocal.net>
2021-06-08 17:10:30 -0400

    Initial Commit

---
+++ flask_tutorial.py
@@ -0,0 +1,17 @@
+
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/Dude')
+def hello_world():
+   print("Im printing..", flush=True)
+   return render_template('index.html')
+
+@app.route('/Dudett')
+def hello_dudett():
+   return 'Hello Dudett'
+
+if __name__ == '__main__':
+   app.run()
